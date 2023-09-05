a826dc569a5ef81a3184ea5a3a27d6cb1632afcc
erlangera <1638442068@qq.com>
2023-09-05 22:11:56 +0800

    优化

--- src/App.js
+++ src/App.js
@@ -11,73 +11,73 @@ const initialBottles = [
     name: "Calm",
     color: "#EDF6F5",
     percent: 0,
-    poster: "serene chamomile"
+    poster: "serene chamomile",
   },
   {
     name: "Reflective",
     color: "#ECCDFF",
     percent: 0,
-    poster: "musing white tea"
+    poster: "musing white tea",
   },
   {
     name: "Grateful",
     color: "#DDFFF7",
     percent: 0,
-    poster: "lovely honey"
+    poster: "lovely honey",
   },
   {
     name: "Happy",
     color: "#FFFAE9",
     percent: 0,
-    poster: "cheery jasmine"
+    poster: "cheery jasmine",
   },
   {
     name: "Excited",
     color: "#FFF6D7",
     percent: 0,
-    poster: "brilliant peppermint"
+    poster: "brilliant peppermint",
   },
   {
     name: "Tired",
     color: "#CCC",
     percent: 0,
-    poster: "restful lavender"
+    poster: "restful lavender",
   },
   {
     name: "Stressed",
     color: "#FFD8D8",
     percent: 0,
-    poster: "pressured basil"
+    poster: "pressured basil",
   },
   {
     name: "Sad",
     color: "#D6EDFF",
     percent: 0,
-    poster: "gloomy cinnamon"
+    poster: "gloomy cinnamon",
   },
   {
     name: "Lonely",
     color: "#D9DFE3",
     percent: 0,
-    poster: "hug-in-a-mug vanilla"
+    poster: "hug-in-a-mug vanilla",
   },
   {
     name: "Angry",
     color: "#FFC5B2",
     percent: 0,
-    poster: "peeved liquorice"
+    poster: "peeved liquorice",
   },
   {
     name: "Confused",
     color: "#CFD4FF",
     percent: 0,
-    poster: "baffled rosemary"
+    poster: "baffled rosemary",
   },
   {
     name: "Uncertain",
     color: "url(#paint0_linear_46_2185)",
     percent: 0,
-    poster: "mystery blend"
+    poster: "mystery blend",
   },
 ];
 
